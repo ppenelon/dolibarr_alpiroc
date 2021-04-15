3a67315d5b45bad88c8eb928d710428792e53301
Pierre Penelon <penelon.pierre@gmail.com>
2021-04-15 16:49:01 +0200

    Set 'default' alpiroc profile on empty profile selected

--- alpiroc/sql/alpiroc.class.php
+++ alpiroc/sql/alpiroc.class.php
@@ -227,7 +227,7 @@ function fetchValueFromProfil($col,$profil)
             if ($this->db->num_rows($resql))
             {
                 $obj = $this->db->fetch_object($resql);  
-				$this->name = $obj->profilalpiroc;
+		$this->name = empty($obj->profilalpiroc) ? 'default' : '';
             }
             $this->db->free($resql);
 
